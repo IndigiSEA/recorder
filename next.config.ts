ab987686ef4602a48ff833344e660731d3887cba
Jona0002 <jona0002@student.monash.edu>
2026-09-06 01:45:09 +0800

    Fixes precaching with additionalPrecacheEntries

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,14 @@ import withSerwistInit from "@serwist/next"
 import type { NextConfig } from "next"
 import createNextIntlPlugin from "next-intl/plugin"
 
+const revision = crypto.randomUUID()
+
 const withSerwist = withSerwistInit({
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
   cacheOnNavigation: true,
   reloadOnOnline: false,
+  additionalPrecacheEntries: [{ url: "/recorder", revision }],
   disable: process.env.NODE_ENV === "development",
 })
 
