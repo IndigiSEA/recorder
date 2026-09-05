df47a41a26e718b8ffb525011cffd844246eecea
Jona0002 <jona0002@student.monash.edu>
2026-09-05 18:59:21 +0800

    Removes unused cookies from request.ts

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,15 +1,7 @@
 import { getRequestConfig } from "next-intl/server"
-import { cookies } from "next/headers"
 
 export default getRequestConfig(async () => {
-  const store = await cookies()
-  const supportedLocales = ["en", "ms"] as const
-  type SupportedLocale = (typeof supportedLocales)[number]
-
-  const cookieLocale = store.get("locale")?.value
-  const locale: SupportedLocale = supportedLocales.includes(cookieLocale as SupportedLocale)
-    ? (cookieLocale as SupportedLocale)
-    : "en"
+  const locale = "en" // Default locale
 
   return {
     locale,
